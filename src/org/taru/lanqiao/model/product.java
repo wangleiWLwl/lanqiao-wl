b1129b34a717b519f5dc6aa36b3a7385206446c5
wanglei <932410335@qq.com>
2019-07-29 16:05:05 +0800

    我编写了一个商品实体类，用以添加商品页面

---
+++ src/org/taru/lanqiao/model/product.java
@@ -0,0 +1,132 @@
+package org.taru.lanqiao.model;
+
+public class product {
+    private String productId;
+    private String productName;
+    private String productPhoto;
+    private String productCode;
+    private String productStandard;
+    private String productSmallUnit;
+    private String productSmallPrice;
+    private String productLargerUnit;
+    private String productLargerPrice;
+    private String productCategoryId;
+    private String productBrandId;
+    private String productIsSale;
+    private String productIsLack;
+    private String productStatus;
+    private String productOrder;
+    private String productDateTime;
+    private String productLargerStandard;
+    private String productSuggestPrice;
+
+
+    public String getProductId() {
+        return productId;
+    }
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public String getProductName() {
+        return productName;
+    }
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public String getProductPhoto() {
+        return productPhoto;
+    }
+    public void setProductPhoto(String productPhoto) {
+        this.productPhoto = productPhoto;
+    }
+    public String getProductCode() {
+        return productCode;
+    }
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+    public String getProductStandard() {
+        return productStandard;
+    }
+    public void setProductStandard(String productStandard) {
+        this.productStandard = productStandard;
+    }
+    public String getProductSmallUnit() {
+        return productSmallUnit;
+    }
+    public void setProductSmallUnit(String productSmallUnit) {
+        this.productSmallUnit = productSmallUnit;
+    }
+    public String getProductSmallPrice() {
+        return productSmallPrice;
+    }
+    public void setProductSmallPrice(String productSmallPrice) {
+        this.productSmallPrice = productSmallPrice;
+    }
+    public String getProductLargerUnit() {
+        return productLargerUnit;
+    }
+    public void setProductLargerUnit(String productLargerUnit) {
+        this.productLargerUnit = productLargerUnit;
+    }
+    public String getProductLargerPrice() {
+        return productLargerPrice;
+    }
+    public void setProductLargerPrice(String productLargerPrice) {
+        this.productLargerPrice = productLargerPrice;
+    }
+    public String getProductCategoryId() {
+        return productCategoryId;
+    }
+    public void setProductCategoryId(String productCategoryId) {
+        this.productCategoryId = productCategoryId;
+    }
+    public String getProductBrandId() {
+        return productBrandId;
+    }
+    public void setProductBrandId(String productBrandId) {
+        this.productBrandId = productBrandId;
+    }
+    public String getProductIsSale() {
+        return productIsSale;
+    }
+    public void setProductIsSale(String productIsSale) {
+        this.productIsSale = productIsSale;
+    }
+    public String getProductIsLack() {
+        return productIsLack;
+    }
+    public void setProductIsLack(String productIsLack) {
+        this.productIsLack = productIsLack;
+    }
+    public String getProductStatus() {
+        return productStatus;
+    }
+    public void setProductStatus(String productStatus) {
+        this.productStatus = productStatus;
+    }
+    public String getProductOrder() {
+        return productOrder;
+    }
+    public void setProductOrder(String productOrder) {
+        this.productOrder = productOrder;
+    }
+    public String getProductDateTime() {
+        return productDateTime;
+    }
+    public void setProductDateTime(String productDateTime) {
+        this.productDateTime = productDateTime;
+    }
+    public String getProductLargerStandard() {
+        return productLargerStandard;
+    }
+    public void setProductLargerStandard(String productLargerStandard) {
+        this.productLargerStandard = productLargerStandard;
+    }
+    public String getProductSuggestPrice() {
+        return productSuggestPrice;
+    }
+    public void setProductSuggestPrice(String productSuggestPrice) {
+        this.productSuggestPrice = productSuggestPrice;
+    }
+}
